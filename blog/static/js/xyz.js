01e0497a671f6cda32dde1cabfce84c7d132cc12
preetgur <preetgur0137@gmail.com>
2020-08-11 13:58:07 +0530

    configuring static and media files

---
+++ blog/static/js/xyz.js
@@ -0,0 +1,2 @@
+let name = "Gurpreet singh";
+let rollno = 151130;
--- paytoservice/urls.py
+++ paytoservice/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+# for media files
+from django.conf.urls.static import static
+from django.conf import settings
+
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('blog.urls')),  
 ]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+#when debug is false
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
